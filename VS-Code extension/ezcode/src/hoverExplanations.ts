18fcd3f71fa319406ebe320f90a48f92e09ec483
Joseph Hamilton <140653231+OneBlueOx@users.noreply.github.com>
2023-07-26 22:13:40 -0500

    hover+icon

---
+++ VS-Code extension/ezcode/src/hoverExplanations.ts
@@ -0,0 +1,8 @@
+const hoverExplanations: Record<string, string> = {
+    print: 'The "print" keyword is used to display output to the console.',
+    write: 'The "write" keyword is used to write text to a file.',
+    // Add more explanations for other keywords here
+  };
+  
+  export default hoverExplanations;
+  
