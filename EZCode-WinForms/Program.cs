c5b855891581b72b287c871b2699af55fe775838
Joseph Hamilton <jlhamilton0408@gmail.com>
2023-10-30 18:58:55 -0500

    Fixed WinForms...

--- EZCode-WinForms/Program.cs
+++ EZCode-WinForms/Program.cs
@@ -1,9 +1,17 @@
-namespace EZPlayer_WinForms {
-    internal static class Program {
-        [STAThread] static void Main() {
+using EZCode;
+
+namespace EZPlayer_WinForms
+{
+    internal static class Program
+    {
+        static void Main()
+        {
             ApplicationConfiguration.Initialize();
-            EZCode.EZProj ez = new EZCode.EZProj(@"C:\Path\To\EZProj\File.ezproj");
-            Application.Run(new EZCode.EZPlayer.Player(ez));
+            EzCode ez = new EzCode();
+            string code = "Code...";
+            ez.Code = code;
+            EZProj proj = new EZProj(ez);
+            Application.Run(new EZCode.EZPlayer.Player(proj));
         }
     }
 }
