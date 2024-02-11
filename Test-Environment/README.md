396bed3952b9ed2186dbd486d925144e317732ea
Joseph Hamilton <jlhamilton0408@gmail.com>
2024-02-11 12:56:05 -0600

    Starting work on EZCode v3.0.0

--- Test-Environment/README.md
+++
@@ -1,4 +0,0 @@
-# EZCode Test Environment
-  This is EZCode test Environment. 
-  
-  It is used to check the newest versions of EZCode before they are released
---
+++ TestEnv/Program.cs
@@ -0,0 +1,16 @@
+﻿using EZCodeLanguage;
+
+EZCode ezcode = new EZCode();
+
+string code = """
+    if ! false : var a => input keys
+    """;
+
+string[] tokens = ezcode.Tokenize(code).Select(x => x.Type.ToString()).ToArray();
+
+Console.WriteLine(code + "\n--------------------\n--------------------\n" + 
+    string.Join(", ", tokens)
+    .Replace(", NewLine, ", "NewLine")
+    .Replace("NewLine", "\n")
+    .Replace(", \n", "\n")
+    .Replace("\n, ", "\n"));
